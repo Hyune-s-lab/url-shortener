cf9480ab889a18b1830327e4297fcccb6e69649b
Hyune-c <choi8608@gmail.com>
2024-06-10 14:21:14 +0900

    [phase2] testcontainer 구성: test 적용

--- apps/url-shortener-api/src/test/kotlin/com/hyunec/urlshortenerapi/AbstractUrlShortenerApiApplicationTests.kt
+++ apps/url-shortener-api/src/test/kotlin/com/hyunec/urlshortenerapi/AbstractUrlShortenerApiApplicationTests.kt
@@ -6,6 +6,7 @@ import net.datafaker.Faker
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.containers.MySQLContainer
 
 @ActiveProfiles(value = ["test"])
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,5 +18,19 @@ abstract class AbstractUrlShortenerApiApplicationTests {
     companion object: KLogging() {
         @JvmStatic
         protected val datafaker = Faker()
+
+        @JvmField
+        val container = MySQLContainer("mysql:8.0.33").apply {
+            withDatabaseName("url_shortener")
+            withUsername("root")
+            withPassword("u1234")
+        }
+
+        init {
+            container.start()
+            System.setProperty("spring.datasource.url", container.jdbcUrl)
+            System.setProperty("spring.datasource.username", container.username)
+            System.setProperty("spring.datasource.password", container.password)
+        }
     }
 }
